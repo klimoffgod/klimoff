9bcd6e6c03f30f12c94ddb16b84063801525323e
klimoff <klimoff@tut.by>
2022-07-30 19:44:48 +0300

    install docker and kubik to clear deb 11

---
+++ k3s_docker.sh
@@ -0,0 +1,9 @@
+apt-get update && apt-get upgrade -y && apt install aptitude sudo mc htop nano wget
+aptitude install -y  ca-certificates curl lsb-release net-tools mc apt-transport-https gnupg2 software-properties-common git
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && aptitude update && aptitude install -y docker.io  docker-ce docker-ce-cli  docker-compose
+apt autoremove -y
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube 
+sudo chmod 755 /usr/local/bin/minikube 
+minikube start --vm-driver=docker --force
+
