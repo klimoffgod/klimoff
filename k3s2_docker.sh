6d12829dddc6237fb56fc416c6c542a84e74ae3e
klimoff <klimoff@tut.by>
2022-08-02 00:02:50 +0300

    install docker and kubik 22 to clear debian 11

---
+++ k3s2_docker.sh
@@ -0,0 +1,13 @@
+apt-get update && apt-get upgrade -y && apt install aptitude sudo mc htop nano wget curl
+sudo apt update && sudo apt install -y apt-transport-https software-properties-common screen 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+apt autoremove -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && aptitude update && aptitude install -y docker.io  docker-ce docker-ce-cli  docker-compose
+minikube start --vm-driver=docker --force
+screen -d -m kubectl proxy --address='0.0.0.0' --disable-filter=true
+
